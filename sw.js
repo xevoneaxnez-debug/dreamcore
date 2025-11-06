1f5c9d106d9948bd8554fcd1a712234818173022
xevoneaxnez-debug <xevoneaxnez@gmail.com>
2025-11-07 01:39:06 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,36 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("dreamcore").then((cache) => {
-      return cache.addAll(["./", "./index.html"]);
-    })
+const CACHE_NAME = 'dreamcore-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon.svg',
+  './manifest.json',
+  './sw.js'
+];
+
+// Install Service Worker and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate Service Worker and remove old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch files from cache first
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
